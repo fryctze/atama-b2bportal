d73f69175cc74044f6a6717cf13f6d7f55157498
Fikry C Farenza <vickyfarenza@gmail.com>
2023-12-27 21:41:39 +0700

    :sparkles: Split into laravel components :art:

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
-    return view('template/index');
-});
+Route::get('/', [DashboardController::class, 'index']);
